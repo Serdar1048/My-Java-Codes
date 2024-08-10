32d6974f62bfca94bf290925f731592c460eb8a8
Serdar Dedebaş <129222249+Serdar1048@users.noreply.github.com>
2024-08-10 16:44:36 +0300

    Add files via upload

---
+++ 06.Loops/src/test.java
@@ -0,0 +1,66 @@
+import javax.swing.plaf.multi.MultiInternalFrameUI;
+
+public class test {
+
+	public static void main(String[] args) {
+		// For Loop
+//		int total = 0;
+//		for(int i = 1; i<=10 ; i++) {
+//			if (i%2==1) {
+//				total += i;
+//			}
+//		}
+//		System.out.println(total);
+
+//		for (int i = 1; i<=20;i++) {
+//			if (i%2==0) {
+//				System.out.println("a");
+//			}else {
+//				System.out.println(i);
+//			}
+//		}
+
+		// While Loop
+//		int i = 1;
+////		int total = 0;
+//		while (i <= 20) {
+//			if (i % 2 == 0) {
+//				System.out.println("a");;
+//			}else {
+//				System.out.println(i);
+//			}
+//			i++;
+//		}
+////		System.out.println(total);
+
+		// Break 1-2-3-asd
+//		int istedigimSayi;
+//		for (int i = 1; i <= 10; i++) {
+//			if (i == 4) {
+//				istedigimSayi = i;
+//				break;
+//			}
+//			System.out.println(i);
+//		}
+//		System.out.println("asd");
+
+//		//Continue 1-2-3-4-5-6-8-9-10
+//		for (int i = 1 ; i<=10 ; i++) {
+//			if(i==7 || i==8) {
+//				continue;
+//			}
+//			System.out.println(i);
+//		}
+
+		// Çarpım Tablosu Yapımı Basit
+//		for (int i = 1; i <= 10; i++) {
+//			for (int j = 1; j <= 10; j++) {
+//				System.out.println(i + "X" + j + "=" + (i * j));
+//
+//			}
+//			System.out.println("-----------");
+//		}
+
+	}
+
+}
