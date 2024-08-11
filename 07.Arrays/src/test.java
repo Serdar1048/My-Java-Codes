77c445747da8a4a3e052fd925bc463021ff81aa7
Serdar Dedebaş <129222249+Serdar1048@users.noreply.github.com>
2024-08-11 14:09:59 +0300

    Add files via upload

---
+++ 07.Arrays/src/test.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+
+import javax.print.attribute.SetOfIntegerSyntax;
+
+public class test {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		// String Arrays
+//		String[] isimDizisi = { "Ali", "veli", "Enes", "Yakup", "bilal", "Oğuz" };
+//		for (int i = 0; i < isimDizisi.length; i++) {
+//			System.out.println(isimDizisi[i]);
+//		}
+
+		// Integer Arrays
+////		int[] sayilar = {15, 125, 14, 12, 4, 545};
+//		int[] sayilar = new int[6]; // Bir dizi oluşturucam ve bu dizinin eleman sayısı 6 olucak
+//		sayilar[0] = 15;
+//		sayilar[1] = 125;
+//		sayilar[2] = 14;
+//		sayilar[3] = 12;
+//		sayilar[4] = 4;
+//		sayilar[5] = 545;
+//
+//		for (int i = 0; i < sayilar.length; i++) {
+//			System.out.println(sayilar[i]);
+//		}
+
+		// Char Arrays
+//		char[] karakterDizi = { 'e', 'n', 'e', 's' };
+//		for (int i = 0; i < karakterDizi.length; i++) {
+//			System.out.println(karakterDizi[i]);
+//		}
+//		System.out.println(karakterDizi);
+
+		// Double Arrays
+//		double[] doubleDizi = { 12.6, 65.2, 76.1, 5.6, 2 };
+//		for (int i = 0; i < doubleDizi.length; i++) {
+//			System.out.println(doubleDizi[i]);
+//		}
+
+		// Dizi elemanlarını kullanıcıdan alma
+//		Scanner scanner = new Scanner(System.in);
+//		
+//		System.out.print("Dizide kaç eleman olmasını istediğinizi yazınız: ");
+//		int elemanSayisi = scanner.nextInt();
+//		scanner.nextLine();
+//		String[] isimler = new String[elemanSayisi];
+//		
+//		for (int i = 0 ; i<elemanSayisi;i++) {
+//			System.out.println("Dizinin " + i + ". index değerini giriniz: ");
+//			isimler[i] = scanner.nextLine();
+//			
+//		}
+//		for (int i = 0;i<elemanSayisi;i++) {
+//			System.out.println(isimler[i]);
+//		}
+
+		// Foreach döngüsü
+//		String[] isimler = {"Enes", "Yakup", "Bilal", "Yasin", "Eren"};
+//		for (String eleman : isimler) {
+//			System.out.println(eleman);
+//		}
+
+//		int[] rakamlar = {0,1,2,3,4,5,6,7,8,9};
+//		for(int rakam : rakamlar) {
+//			System.out.println(rakam);
+//		}
+
+		// Multidimensional Array
+//		int[][] tablo = new int[][] { { 1, 2, 3 }, { 2, 2, 2 }, { 8, 9, 10 } };
+//		int[][] tablo2 = new int[5][3];
+//		tablo2[0][0] = 1;
+//		tablo2[0][1] = 2;
+//
+//		for (int i = 0; i < tablo.length; i++) {
+//			for (int j = 0; j < tablo[0].length; j++) {
+////				System.out.println(tablo[i][j]);
+//			}
+//		}
+//
+//		// For Each (Multidimensional Array)
+//		for (int[] sutun : tablo) {
+//			for (int value : sutun) {
+//				System.out.println(value);
+//			}
+//		}
+
+	}
+}
