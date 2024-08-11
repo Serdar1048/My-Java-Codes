8b627f1fcafff19b9ba38bedb5cb7a44936aab33
Serdar Dedebaş <129222249+Serdar1048@users.noreply.github.com>
2024-08-11 14:10:34 +0300

    Add files via upload

---
+++ 08.Methods/src/test.java
@@ -0,0 +1,42 @@
+
+public class test {
+
+	public static void main(String[] args) {
+
+//		isimYazdir();
+//		topla(17, 5);
+//		isimSoyisimYazdir("a", "b");
+
+//		int a = carp(5, 2);
+//		System.out.println(a);
+//		System.out.println(carp(2, 3));
+
+		String ab = isimDondur("asd");
+		System.out.println(ab);
+		System.out.println(isimDondur("asdfg"));
+
+	}
+
+//	public static void isimYazdir() {
+//		System.out.println("abcww");
+//	}
+//	
+//	public static void topla(int sayi1, int sayi2) {
+//		int sonuc = sayi1 + sayi2;
+//		System.out.println("Sonuç: " + sonuc);
+//	}
+//	
+//	public static void isimSoyisimYazdir(String isim, String soyisim){
+//		System.out.println("isim ve soyisim : " + isim + " " + soyisim);
+//	}
+
+//	public static int carp(int sayi1, int sayi2) {
+//		int sonuc = sayi1 * sayi2;
+//		return sonuc;
+//	}
+
+	public static String isimDondur(String isim) {
+		return isim;
+	}
+
+}
