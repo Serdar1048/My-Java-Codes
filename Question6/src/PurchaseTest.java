e2e335825749583b1ede3afd1ee0fa945cde8707
Serdar Dedebaş <129222249+Serdar1048@users.noreply.github.com>
2024-08-15 17:34:37 +0300

    Add files via upload

---
+++ Question6/src/PurchaseTest.java
@@ -0,0 +1,26 @@
+/**
+ * Write a class Purchase. The class has the following attributes:
+ * • The number of items purchased
+ * • The price of each item
+ * The class has the following methods:
+ * • A method to set the number of items and the price per item
+ * • A method to calculate and return the total cost of the purchase
+ * • A method to calculate and return the total cost of the purchase including a 7 percent sales tax
+ * Oranges: 10 for $2.99
+ * Eggs: 12 for $1.69
+ * Apples: 3 for $1.00
+ * Watermelons: $4.39 each
+ * Bagels: 6 for $3.50
+ * Then calculate the cost of each of the following five items and the total bill:
+ * 2 dozen oranges
+ * 3 dozen eggs
+ * 20 apples
+ * 2 watermelons
+ * 1 dozen bagels
+ * */
+public class PurchaseTest
+{
+    public static void main(String[] args)
+    {
+    }
+}
